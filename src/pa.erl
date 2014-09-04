071a0d23f61618e19915ded7812be3fa07a8fe39
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2014-09-04 17:16:57 +0200

    Implement binary and ternary partial applications

--- src/pa.erl
+++ src/pa.erl
@@ -18,13 +18,15 @@ nullary(Fun, Args) -> nary(0, Fun, Args).
 
 unary(Fun, Args) -> nary(1, Fun, Args).
 
-binary(Fun, Args) ->
-    fun() -> ok end.
+binary(Fun, Args) -> nary(2, Fun, Args).
 
-ternary(Fun, Args) ->
-    fun() -> ok end.
+ternary(Fun, Args) -> nary(3, Fun, Args).
 
 nary(0 = N, Fun, Args) when is_function(Fun, length(Args) + N) ->
     fun() -> erlang:apply(Fun, Args) end;
 nary(1 = N, Fun, Args) when is_function(Fun, length(Args) + N) ->
-    fun(A1) -> erlang:apply(Fun, Args ++ [A1]) end.
+    fun(A1) -> erlang:apply(Fun, Args ++ [A1]) end;
+nary(2 = N, Fun, Args) when is_function(Fun, length(Args) + N) ->
+    fun(A1, A2) -> erlang:apply(Fun, Args ++ [A1, A2]) end;
+nary(3 = N, Fun, Args) when is_function(Fun, length(Args) + N) ->
+    fun(A1, A2, A3) -> erlang:apply(Fun, Args ++ [A1, A2, A3]) end.
