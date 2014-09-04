f7111c9b7d4d683401730452ec48cb9a8bc7c114
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2014-09-04 14:15:20 +0200

    Trim the usage examples

--- src/pa.erl
+++ src/pa.erl
@@ -9,10 +9,6 @@
 
 %% Usage examples:
 %%
-%%   partial:unary(fun mymod:myfun/4, [1,2,3])
-%%   partial:binary(fun mymod:myfun/4, [1,2])
-%%   partial:nary(4, fun mymod:myfun/7, [1,2,3])
-%%
 %%   pa:unary(fun mymod:myfun/4, [1,2,3])
 %%   pa:binary(fun mymod:myfun/4, [1,2])
 %%   pa:nary(4, fun mymod:myfun/7, [1,2,3])
